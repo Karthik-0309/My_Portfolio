e4387f37f00df8d99d72b39f7cd94ebac8da021c
Karthik kashyap R P <karthikashyap.0903@gmail.com>
2025-01-25 00:21:57 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,16 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  disable: process.env.NODE_ENV === "development" || process.env.GITHUB_ACTIONS === "true", // Disable PWA in development and GitHub Pages
 });
+
+// Determine if the app is being built for GitHub Pages
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isGitHubPages ? "/My_Portfolio" : "", // Set basePath for GitHub Pages
+  assetPrefix: isGitHubPages ? "/My_Portfolio/" : "", // Set assetPrefix for static assets
 };
 
 module.exports = withPWA(nextConfig);
