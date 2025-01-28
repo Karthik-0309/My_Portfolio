2d419c129232ad7d10ffd4db287bcaf197025fbf
Karthik kashyap R P <karthikashyap.0903@gmail.com>
2025-01-27 20:09:46 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,11 @@ const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isGitHubPages ? "/My_Portfolio" : "",
+  basePath: isGitHubPages ? "/My_Portfolio" : "", // Ensure this matches your GitHub repository name
   assetPrefix: isGitHubPages ? "/My_Portfolio/" : "",
   output: "export", // Required for GitHub Pages
+  trailingSlash: true, // Ensure all paths end with a slash for GitHub Pages compatibility
 };
 
 module.exports = nextConfig;
+
